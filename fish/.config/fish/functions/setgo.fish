3f0884f8619ea0a2bb9a7d3087bb26d14f629499
Rustam Zagirov <stammru@gmail.com>
2019-03-19 20:12:21 +0300

    update go version

--- fish/.config/fish/functions/setgo.fish
+++ fish/.config/fish/functions/setgo.fish
@@ -1,16 +1,18 @@
 function setgo
 	switch (echo $argv[1])
 	case 1.10
-		set version 1.10.3
+		set go_version 1.10.3
 	case 1.11
-		set version 1.11.4
+		set go_version 1.11.5
+	case 1.12
+		set go_version 1.12.1
 	case tip
-		set version tip
+		set go_version tip
 	case '*'
 		echo "Don't have this version"
 		return 1
 	end
-	ln -nfs /usr/local/go_versions/$version /usr/local/go_versions/current
+	ln -nfs /usr/local/go_versions/$go_version /usr/local/go_versions/current
 end
 
-complete -c setgo -x -a "1.10 1.11 tip"
+complete -c setgo -x -a "1.10 1.11 1.12 tip"
