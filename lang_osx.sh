38d90119d64bce46c5699ce7ec362c3c3a85c330
Rustam Zagirov <stammru@gmail.com>
2023-03-24 12:04:56 +0300

    reduce language in tmux

--- lang_osx.sh
+++ lang_osx.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | egrep -w 'KeyboardLayout Name' | cut -f2 -d= | cut -f2 -d'"' | cut -f1 -d" "
+defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | egrep -w 'KeyboardLayout Name' | cut -f2 -d= | tr -d '" ;' | head -c 2
