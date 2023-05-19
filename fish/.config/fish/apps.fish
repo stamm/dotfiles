358a9b93e9ab9bb338fa2d734e2e1f0ca89a2e5b
Rustam Zagirov <stammru@gmail.com>
2023-05-19 12:32:25 +0300

    fish: add complections, add PATH for psql

--- fish/.config/fish/apps.fish
+++ fish/.config/fish/apps.fish
@@ -1,5 +1,16 @@
 # source /usr/local/etc/grc.fish
-source /usr/local/opt/asdf/asdf.fish
+if test -d (brew --prefix)"/share/fish/completions"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# psql
+fish_add_path /opt/homebrew/opt/libpq/bin
+
 status --is-interactive; and source (jump shell fish | psub)
 starship init fish | source
 eval (command direnv hook fish | source)
