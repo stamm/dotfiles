5c981f7107c702dfad0978871053419bda7918b0
Rustam Zagirov <stammru@gmail.com>
2023-05-25 10:37:51 +0300

    remove more symbols for show current language layout

--- lang_osx.sh
+++ lang_osx.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | egrep -w 'KeyboardLayout Name' | cut -f2 -d= | tr -d '" ;' | head -c 2
+defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | egrep -w 'KeyboardLayout Name' | cut -f2 -d= | tr -d '." ;' | head -c 2
